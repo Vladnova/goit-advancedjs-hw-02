50b9011671ea2b6f705e6f850a72ed199ea87a6f
Vladyslav Novalenko <vladyslav.novalenko@infotech.gov.ua>
2023-11-10 13:16:37 +0200

    finished first home work

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,44 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
+
+
+  const refs = {
+    body: document.querySelector('body'),
+    btnStart: document.querySelector('[data-start]'),
+    btnStop : document.querySelector('[data-stop]')
+  }
+
+
+  refs.btnStart.addEventListener('click', handlerStart);
+  refs.btnStop.addEventListener('click', handlerStop);
+
+  let intervalId = null;
+
+  function handlerStart(e) {
+    let currentColor = getRandomHexColor();
+    let bodyBgColor = refs.body.style;
+
+    bodyBgColor.backgroundColor= currentColor;
+    intervalId = setInterval(()=>{
+      currentColor = getRandomHexColor();
+      bodyBgColor.backgroundColor = currentColor;
+    }, 1000)
+
+    refs.btnStop.disabled = false;
+    toogleDisabled(e)
+
+  }
+
+  function handlerStop(e) {
+    clearInterval(intervalId)
+    refs.btnStart.disabled = false;
+    toogleDisabled(e)
+  }
+
+  function toogleDisabled(e) {
+    e.target.disabled =!e.target.disabled
+  }
+
+
 
