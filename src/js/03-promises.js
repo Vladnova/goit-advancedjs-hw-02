8c297736834b336783cc09f58e9b72a91fad86f1
Vladyslav Novalenko <vladyslav.novalenko@infotech.gov.ua>
2023-11-13 23:37:19 +0200

    finished third home work

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,57 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const refs = {
+  form: document.querySelector('.form'),
+};
+
+refs.form.addEventListener('submit', handlerSubmit);
+
+function handlerSubmit(e) {
+  e.preventDefault();
+
+  const resultData = {};
+
+  const formData = new FormData(refs.form);
+
+  formData.forEach((value, key) => (resultData[key] = +value));
+
+  let { delay, step, amount } = resultData;
+
+  for (let i = 1; i <= amount; i += 1) {
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        iziToast.success({
+          title: 'OK',
+          message: `✅ Fulfilled promise ${position} in ${delay}ms`,
+          position: 'topRight',
+        });
+      })
+      .catch(({ position, delay }) => {
+        iziToast.error({
+          title: 'Error',
+          message: `❌ Rejected promise ${position} in ${delay}ms`,
+          position: 'topRight',
+        });
+      });
+
+    delay += step;
+  }
+
+  refs.form.reset()
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+  return new Promise((response, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        response({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
+
